9fb768652f704d43b499b3e839a77a804828e5a4
Sukhovolskij.Andrew <sukhovolskijandrewoid@gmail.com>
2019-11-19 22:59:41 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lesson77tabintent/TwoActivity.java
@@ -0,0 +1,13 @@
+package com.example.lesson77tabintent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TwoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+    }
+}
